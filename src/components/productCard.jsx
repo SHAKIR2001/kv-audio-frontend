37dcb965d479fdbced7b1cf71558deb0af43bd44
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-07 13:05:53 +0530

    Add console log to ProductCard component for debugging props 😊

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,6 +1,7 @@
 import "./productCard.css"
 export default function ProductCard(props){ //need to be the function's first letter as capital (must)
     
+    console.log(props)
     return(
         <div>
             <img src="https://img.drz.lazcdn.com/static/lk/p/09fb9b817e9bfc5b0cab3a79288663b9.png_720x720q80.png_.webp"/>
