bfc8aeaa781a5a58aeefe6e90fe83284a15fc730
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-07 12:39:42 +0530

    Refactor ProductCard component and add styling for improved layout and hover effects 😋

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
 
 
   return (
-    <>
-      <ProductCard></ProductCard>
-    </>
+    <div>
+      <ProductCard  name="Audio Setup" price = "3250/-" description="lafbakjhf ajksgjkhag askjfghakjs" img="https://img.drz.lazcdn.com/g/kf/Sd61235d9e2024f8cb5d65134ad22c9e5A.jpg_720x720q80.jpg_.webp"/> 
+
+    </div>
   )
 }
 
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,8 +1,12 @@
+import "./productCard.css"
 export default function ProductCard(){ //need to be the function's first letter as capital (must)
     
     return(
         <div>
-            <h1>Product Card</h1>
+            <img src="https://img.drz.lazcdn.com/static/lk/p/09fb9b817e9bfc5b0cab3a79288663b9.png_720x720q80.png_.webp"/>
+            <span>JBL Audio mini 3</span>
+            <span>LKR. 2250/-</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dolorum.</p>
         </div>
     )
 }
