d3053ea3f3a72d88dc00573a588547627b65fb70
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-07 13:25:49 +0530

    Update ProductCard component to utilize props for dynamic content rendering 😒

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,13 +1,13 @@
 import "./productCard.css"
 export default function ProductCard(props){ //need to be the function's first letter as capital (must)
     
-    console.log(props)
+    
     return(
         <div>
-            <img src="https://img.drz.lazcdn.com/static/lk/p/09fb9b817e9bfc5b0cab3a79288663b9.png_720x720q80.png_.webp"/>
-            <span>JBL Audio mini 3</span>
-            <span>LKR. 2250/-</span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dolorum.</p>
+            <img src={props.img}/> 
+            <span>{props.name}</span>
+            <span>{props.price}</span>   {/* {} idhatkul js use seiyya mudiyum props in moolam(app.jsx file in moolam pass ahum attrtributehalei access seiyalaam)  */}
+            <p>{props.description}</p>  
         </div>
     )
 }
