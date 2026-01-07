891d3faf03a46005d06742026abc8f6502469dde
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-07 13:37:46 +0530

    Update ProductCard description and name for consistency 😎

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div>
       <ProductCard  name="Audio Setup" price = "3250/-" description="lafbakjhf ajksgjkhag askjfghakjs" img="https://img.drz.lazcdn.com/static/lk/p/09fb9b817e9bfc5b0cab3a79288663b9.png_720x720q80.png_.webp"/> 
 
-      <ProductCard  name="JBL Tune 3" price = "5250/-" description="lafbakjhf ajksgjkhag askjfghakjs" img="https://img.drz.lazcdn.com/g/kf/S27aa4cd1bf6f46b487244c93d3655790Q.jpg_720x720q80.jpg_.webp"/> 
+      <ProductCard  name="JBL Mini 3" price = "5250/-" description="JBL Charge Mini 3+ Portable Wireless Bluetooth Speaker With FM Radio JBL" img="https://img.drz.lazcdn.com/g/kf/S27aa4cd1bf6f46b487244c93d3655790Q.jpg_720x720q80.jpg_.webp"/> 
 
     </div>
   )
