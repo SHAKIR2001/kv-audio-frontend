03d5244b1ead9c5ba21f96ec26c4d0f2dd0232b9
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-08 16:59:16 +0530

    fix: update layout and positioning of elements in App component 😁

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ function App() {
 
 
   return (
-    <div>
+    <div className='w-full h-screen flex justify-center items-center'>
 
-    <div className='w-[800px] h-[800px] bg-yellow-200 relative'>
-    <div className='w-[500px] h-[500px] bg-blue-200 relative' >
+    <div className='w-[800px] h-[800px] bg-yellow-200 flex justify-center items-center'>
+    <div className='w-[500px] h-[500px] bg-blue-200 flex flex-col justify-center items-center'>
 
       <div className='w-[50px] h-[50px] bg-red-500'></div>
-      <div className='w-[50px] h-[50px] bg-green-500 absolute right-[15px] bottom-[100px]'></div>
-      <div className='w-[50px] h-[50px] bg-yellow-500 fixed right-[5px] bottom-[15px]'></div>
+      <div className='w-[50px] h-[50px] bg-green-500'></div>
+      <div className='w-[50px] h-[50px] bg-yellow-500'></div>
+
+    </div>
+    </div>
 
     </div>
 
-     
-    </div>
-    </div>
   )
 }
 
