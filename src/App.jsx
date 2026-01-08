e0b6cbb6c42330d71ca4ee9298ab96e2cca6dca2
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-08 12:34:41 +0530

    Add initial styles for test and testTwo classes in App.css 😒

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ function App() {
 
 
   return (
-    <div>
-      <ProductCard  name="Audio Setup" price = "3250/-" description="lafbakjhf ajksgjkhag askjfghakjs" img="https://img.drz.lazcdn.com/static/lk/p/09fb9b817e9bfc5b0cab3a79288663b9.png_720x720q80.png_.webp"/> 
+    <div className='test testTwo'>
 
-      <ProductCard  name="JBL Mini 3" price = "5250/-" description="JBL Charge Mini 3+ Portable Wireless Bluetooth Speaker With FM Radio JBL" img="https://img.drz.lazcdn.com/g/kf/S27aa4cd1bf6f46b487244c93d3655790Q.jpg_720x720q80.jpg_.webp"/> 
+      dfsdd
 
     </div>
   )
