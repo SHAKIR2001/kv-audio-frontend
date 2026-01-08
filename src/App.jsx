962a410e20009009ff603a88dbfcd60586ab1fe4
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-08 16:02:06 +0530

    feat: update App component layout and styling for improved structure 😪

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,17 @@ function App() {
   return (
     <div>
 
-    <div className='w-11 h-11 bg-black' >
+    <div className='w-[800px] h-[800px] bg-yellow-200 relative'>
+    <div className='w-[500px] h-[500px] bg-blue-200 relative' >
+
+      <div className='w-[50px] h-[50px] bg-red-500'></div>
+      <div className='w-[50px] h-[50px] bg-green-500 absolute right-[15px] bottom-[100px]'></div>
+      <div className='w-[50px] h-[50px] bg-yellow-500 fixed right-[5px] bottom-[15px]'></div>
 
     </div>
 
      
-
+    </div>
     </div>
   )
 }
