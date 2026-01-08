9edd65e4c14f1c07787cb56a70a32e120aeb22fd
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-08 14:40:15 +0530

    feat: integrate Tailwind CSS and clean up styles
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed unused CSS classes from App.css and productCard.css.
    - Updated App.jsx to remove class names related to removed styles.
    - Imported Tailwind CSS in index.css.
    - Created a new Vite configuration file for project setup 😒

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import ProductCard from './components/productCard'
 
+
 function App() {
 
 
   return (
-    <div className='test testTwo'>
+    <div className=''>
 
       dfsdd
 
