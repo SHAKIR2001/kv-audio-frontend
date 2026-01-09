b8bbd273da71c737228a0a4050ae3437a9969f1a
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-09 11:38:47 +0530

    feat: enhance App component layout with buttons for navigation 😪

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import './App.css'
 import ProductCard from './components/productCard'
-import { MdBluetoothAudio } from "react-icons/md";
-
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
 function App() {
 
 
   return (
     <div className='w-full h-screen flex'>
 
-      <div className='w-[400px] h-full bg-green-200'></div>
+      <div className='w-[400px] h-full bg-green-200'>
 
-      <div className="w-full bg-red-900"> 
-        <MdBluetoothAudio className="text-[300px] text-white" />
+      <button className="w-full h-[40px] text-[25px] font-bold cursor-pointer bg-red-400 flex justify-center items-center">
+        <BsGraphDown />
+          Dashboard
+      </button>
+
+      <button className="w-full h-[40px] text-[25px] font-bold cursor-pointer flex justify-center items-center">
+          <FaRegBookmark />
+          Bookings
+      </button>
+
+      <button className="w-full h-[40px] text-[25px] font-bold cursor-pointer">
+          Items
+      </button>
+
+      <button className="w-full h-[40px] text-[25px] font-bold cursor-pointer">
+          Users
+      </button>
+
+      <button className="w-full h-[40px] text-[25px] font-bold cursor-pointer">
+          Reviews
+      </button>
+
       </div>
+
+      <div className="w-full bg-red-900"> </div>
       
 
 
