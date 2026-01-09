a9435bfc45275fc51f029ab85a84e120a197cc16
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-09 10:59:45 +0530

    fix: simplify App component layout and remove unused elements 😪

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,11 @@ function App() {
 
 
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
+    <div className='w-full h-screen flex'>
 
-    <div className='w-[800px] h-[800px] bg-yellow-200 flex justify-center items-center'>
-    <div className='w-[500px] h-[500px] bg-blue-200 flex flex-col justify-center items-center'>
+      <div className='w-[300px] h-full bg-green-200'></div>
 
-      <div className='w-[50px] h-[50px] bg-red-500'></div>
-      <div className='w-[50px] h-[50px] bg-green-500'></div>
-      <div className='w-[50px] h-[50px] bg-yellow-500'></div>
 
-    </div>
-    </div>
 
     </div>
 
