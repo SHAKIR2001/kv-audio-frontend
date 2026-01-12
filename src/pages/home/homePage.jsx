8fe17e0e2f61cd4a044d66c8094ad97f8eacaf63
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 13:39:49 +0530

    fix: add header element to HomePage component layout 😋

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,6 +1,7 @@
 export default function HomePage(){
     return(
         <div className='w-full h-screen flex justify-center items-center'>
+            <header className="w-full h-[100px] shadow-xl "></header>
 
         </div>
         
