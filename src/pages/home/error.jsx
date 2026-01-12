333867159037ed6bb6f51ca635d7ceec18537def
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 20:56:35 +0530

    fix: add margin to h1 in ErrorNotFound component for improved spacing 😁

--- src/pages/home/error.jsx
+++ src/pages/home/error.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function ErrorNotFound(){
     return(
     <>
-        <h1>404 Error : Page not found</h1>
+        <h1 className="m-1">404 Error : Page not found</h1>
         <Link className= "bg-[#ebaf3a] p-1" to="/">Go back to home</Link>
 
     </>
