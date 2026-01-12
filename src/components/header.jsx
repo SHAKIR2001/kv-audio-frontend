8c0160987cf56717ec1a28a1d1ec87c6d463372a
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 18:59:24 +0530

    fix: add navigation links to Header component 😎

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Header(){
     return(
-        <header className="w-full h-[100px] shadow-xl "></header>
+        <header className="w-full h-[100px] shadow-xl ">
+            <Link to="/" className=" text-[25px] font-bold">Home</Link>
+            <Link to="/admin" className=" text-[25px] font-bold">Admin</Link>
+
+
+
+
+
+        </header>
     )
 }
