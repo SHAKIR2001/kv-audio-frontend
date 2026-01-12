edc6491dbaebd4871aebbb7e8f130aa71514c5cc
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 20:31:09 +0530

    feat: update HomePage to include routing for Contact, Gallery, and Items components 😁

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,6 +1,9 @@
 import Header from "../../components/header"
 import { Routes, Route } from "react-router-dom"
 import Home from "./home"
+import Contact from "./contact"
+import Gallery from "./gallery"
+import Items from "./items"
 export default function HomePage(){
     return(
     <>
@@ -8,11 +11,9 @@ export default function HomePage(){
         <div className="h-[calc(100vh-100px)] w-full">
            <Routes path="/">
                 <Route path="/" element={<Home/>}/>
-                <Route path="/contact" element={<h1>Contact</h1>}/>
-                <Route path="/gallery" element={<h1>Gallery</h1>}/>
-                <Route path="/items" element={<h1>Items</h1>}/>
-
-
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/gallery" element={<Gallery/>}/>
+                <Route path="/items" element={<Items/>}/>
 
            </Routes>
         </div>
