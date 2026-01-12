e5b9992065246496379caeb5c3bae47418395234
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 13:49:03 +0530

    fix: implement Header component in HomePage layout 😢

---
+++ src/components/header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <header className="w-full h-[100px] shadow-xl "></header>
+    )
+}
--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,7 +1,10 @@
+import Header from "../../components/header"
 export default function HomePage(){
     return(
-        <div className='w-full h-screen flex justify-center items-center'>
-            <header className="w-full h-[100px] shadow-xl "></header>
+        <div className='w-full h-screen flex justify-center '>
+        <Header/>
+
+
 
         </div>
         
