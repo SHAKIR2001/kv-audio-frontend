0209cb01ea9cf116d292c4c7a1112f2be720cca0
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 20:19:29 +0530

    fix: update Home route to render Home component instead of placeholder

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,12 +1,13 @@
 import Header from "../../components/header"
 import { Routes, Route } from "react-router-dom"
+import Home from "./home"
 export default function HomePage(){
     return(
     <>
         <Header/>
         <div className="h-[calc(100vh-100px)] w-full">
            <Routes path="/">
-                <Route path="/" element={<h1>Home</h1>}/>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/contact" element={<h1>Contact</h1>}/>
                 <Route path="/gallery" element={<h1>Gallery</h1>}/>
                 <Route path="/items" element={<h1>Items</h1>}/>
