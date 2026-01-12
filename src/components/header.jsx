94082cf4c016118ad5e1863fbbb937765bffe4d1
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 19:10:00 +0530

    fix: add margin to links in Header component🫣

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return(
         <header className="w-full h-[100px] shadow-xl ">
-            <Link to="/" className=" text-[25px] font-bold">Home</Link>
-            <Link to="/admin" className=" text-[25px] font-bold">Admin</Link>
-            <Link to="/contact" className=" text-[25px] font-bold">Contact</Link>
-            <Link to="/gallery" className=" text-[25px] font-bold">Gallery</Link>
-            <Link to="/items" className=" text-[25px] font-bold">Items</Link>
+            <Link to="/" className=" text-[25px] font-bold m-1">Home</Link>
+            <Link to="/admin" className=" text-[25px] font-bold m-1">Admin</Link>
+            <Link to="/contact" className=" text-[25px] font-bold m-1">Contact</Link>
+            <Link to="/gallery" className=" text-[25px] font-bold m-1">Gallery</Link>
+            <Link to="/items" className=" text-[25px] font-bold m-1">Items</Link>
 
 
 
@@ -15,4 +15,4 @@ export default function Header(){
 
         </header>
     )
-}
+} 
