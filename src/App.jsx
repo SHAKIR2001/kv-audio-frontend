43b79884b5963b3e533d474908842618b6446d5d
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-12 11:18:08 +0530

    remove unnessecorry imports from app.jsx 😁

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import './App.css'
-import ProductCard from './components/productCard'
-import { BsGraphDown } from "react-icons/bs";
-import { FaRegBookmark } from "react-icons/fa";
-import { MdOutlineSpeaker } from "react-icons/md";
-import { FaRegUser } from "react-icons/fa";
-import AdminPage from './components/adminPage';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
 
