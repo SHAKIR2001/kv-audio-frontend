d55ffdd219e673f563e97fed020b2fb26a5343fa
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-13 18:38:37 +0530

    fix: update Testing component layout and styling for better user experience 😒

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -3,10 +3,10 @@ export default function Testing(){
     const [count,setCount] = useState(0)
 
     return(
-        <div className="w-full h-screen bg-red-900">
+        <div className="w-full h-screen flex justify-center items-center flex-col">
             
-            <h1>{count}</h1>
-            <button className="cursor-pointer" onClick={()=>{
+            <h1 className="text-4xl">{count}</h1>
+            <button className="cursor-pointer border rounded bg-red-500 p-1 m-1" onClick={()=>{
                 const newCount = count + 1
                 setCount(newCount)
             }}>Count</button>
