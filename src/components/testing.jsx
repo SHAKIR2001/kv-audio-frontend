9705d1ddd8ae9b0e58041622bf7eddcc7a946e22
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-13 18:29:23 +0530

    fix: correct state management in Testing component's button click handler😪

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react"
 export default function Testing(){
-    const [count,setCount] = useState(0,)
+    const [count,setCount] = useState(0)
 
     return(
         <div className="w-full h-screen bg-red-900">
-          
+            
             <h1>{count}</h1>
             <button className="cursor-pointer" onClick={()=>{
-                count ++
-                console.log(count)
+                
+                setCount(count++)
             }}>Count</button>
-            <h1>{count}</h1>
+            
         </div>
          
     )
