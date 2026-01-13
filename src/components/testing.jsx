abd51fb6beb7931dc977d721d2d0220637d0e3bc
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-13 18:55:39 +0530

    fix: update Testing component to display item count with selected item 😎

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,16 +1,46 @@
 import { useState } from "react"
 export default function Testing(){
     const [count,setCount] = useState(0)
+    const [items, setItems] = useState("Banana")
 
     return(
         <div className="w-full h-screen flex justify-center items-center flex-col">
             
-            <h1 className="text-4xl">{count}</h1>
+            <h1 className="text-4xl m-2">{count + " " +items+"s" }</h1>
             <button className="cursor-pointer border rounded bg-red-500 p-1 m-1" onClick={()=>{
                 const newCount = count + 1
                 setCount(newCount)
             }}>Count</button>
+
+            <div className="flex w-[350px] justify-between items-center m-">
+
+            <button className="cursor-pointer border rounded bg-yellow-500 p-1 m-1" onClick={()=>{
             
+            setItems("Banana")
+
+            }}>Banana</button>
+
+
+            <button className="cursor-pointer border rounded bg-red-500 p-1 m-1" onClick={()=>{
+            
+            setItems("Apple")
+
+            }}>Apple</button>
+
+            <button className="cursor-pointer border rounded bg-orange-500 p-1 m-1" onClick={()=>{
+            
+            setItems("Orange")
+
+            }}>Orange</button>
+
+
+            <button className="cursor-pointer border rounded bg-amber-300 p-1 m-1" onClick={()=>{
+            
+            setItems("Other")
+
+            }}>Other</button>
+
+            </div>
         </div>
          
     )
