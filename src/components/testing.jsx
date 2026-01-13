b30ecf79dbfffaacb1f040c4c30f7fa3522d03a0
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-13 16:13:33 +0530

    fix: replace fragment with div in Testing component for proper structure😪

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,9 +1,9 @@
 export default function Testing(){
     return(
-        <>
+        <div className="w-full h-screen bg-red-900">
           
             <h1>Testing</h1>
-            
-        </>
+
+        </div>
     )
 }
