97b5b26a01f2326807d08eb064bc6d96ab8d7aa9
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-13 16:39:29 +0530

    feat: update Testing component to display and increment count 😒

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,8 +1,13 @@
 export default function Testing(){
+    let count = 0;
     return(
         <div className="w-full h-screen bg-red-900">
           
-            <h1>Testing</h1>
+            <h1>{count}</h1>
+            <button className="cursor-pointer" onClick={()=>{
+                count ++
+                console.log(count)
+            }}>Count</button>
 
         </div>
     )
