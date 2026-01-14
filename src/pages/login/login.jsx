f77d82fb432962fb2b114bc8518a67a6e65fc798
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-14 15:37:01 +0530

    add redirect after successful login based on user role 😎

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -18,6 +18,12 @@ export default function LoginPage(){
             (res)=>{
                 console.log(res)
                 toast.success("Login Success")
+                const user = res.data.user 
+                if(user.role === "admin"){
+                    window.location.href = "/admin/"
+                }else{
+                    window.location.href = "/"
+                }
             }
         ).catch(
             (err)=>{
