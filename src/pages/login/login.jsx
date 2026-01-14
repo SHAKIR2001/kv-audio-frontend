91401c89d21d0f2fae69601b15a48ff565bafb97
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-14 10:12:39 +0530

    fix: enhance layout of LoginPage component for better alignment and structure 😒

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -10,7 +10,9 @@ export default function LoginPage(){
       <div className="w-full h-screen flex justify-center items-center bg-picture ">
 
 
-        <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl">
+        <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex justify-center items-center relative">
+            
+            <img className="absolute top-1 " src="logo.png"/>
 
         </div>
 
