69cb61f891adc8b323232cf4ef787a1e2d727a0c
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-14 13:08:50 +0530

    add response logging in handleOnSubmit function 😁

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -13,7 +13,15 @@ export default function LoginPage(){
         axios.post("http://localhost:300/api/users/login", {
             email : email,
             password : password
-        })
+        }).then(
+            (res)=>{
+                console.log(res)
+            }
+        ).catch(
+            (err)=>{
+                console.log(err)
+            }
+        )
     
     }
 
