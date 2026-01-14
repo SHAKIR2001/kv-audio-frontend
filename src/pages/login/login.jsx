31913032edd217cf778961ce69aab6fb39b7f7d1
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-14 09:45:13 +0530

    add username and password as useState for the login page😎

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react"
 export default function LoginPage(){
+
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    
     return(
+
       <div>
 
 
         <h1>Login</h1>
 
 
-        
+
       </div>
     )
 }
