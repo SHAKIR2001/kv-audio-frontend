2e08d9f76a188c5ce9c88a7565db79db11dcb983
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-14 09:46:46 +0530

    fix: update layout of LoginPage component for better styling 😁

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,10 +3,10 @@ export default function LoginPage(){
 
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
-    
+
     return(
 
-      <div>
+      <div className="w-full h-screen bg-amber-200">
 
 
         <h1>Login</h1>
