245f5de29b93235efe3b3dd087f9b9af042d1c4b
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-17 12:45:14 +0530

    update layout of AdminItemsPage for improved icon positioning 😁

--- src/pages/admin/adminItemsPage.jsx
+++ src/pages/admin/adminItemsPage.jsx
@@ -2,8 +2,8 @@ import { CiCirclePlus } from "react-icons/ci";
 export default function AdminItemsPage(){
 
     return(
-        <div className="w-full h-full bg-amber-200">
-            <div><CiCirclePlus /></div>
+        <div className="w-full h-full relative">
+            <CiCirclePlus className="text-[100px] absolute bottom-15 right-20"/>
 
             
             
