ea620c5a6fd48999f800f9018e664a96228fa66f
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-17 14:24:14 +0530

    style: center align inputs in AddProduct component for improved layout 😪

--- src/pages/admin/addProductPage.jsx
+++ src/pages/admin/addProductPage.jsx
@@ -2,7 +2,7 @@ export default function AddProduct(){
     return(
         <div className="w-full h-full flex flex-col items-center">
             <h1>Add items</h1>
-            <div className="w-[400px]  border flex flex-col ">
+            <div className="w-[400px]  border flex flex-col items-center ">
                 <input type="text" placeholder="Product Key"/>
                 <input type="text" placeholder="Product Name"/>
                 <input type="text" placeholder="Product Price"/>
