fd37a0f856b00b169dcea9771022f7c2a3957d0d
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-17 14:09:43 +0530

    style: update layout of AddProduct component for better alignment😋

--- src/pages/admin/addProductPage.jsx
+++ src/pages/admin/addProductPage.jsx
@@ -1,8 +1,8 @@
 export default function AddProduct(){
     return(
-        <div className="w-full h-full">
+        <div className="w-full h-full flex flex-col items-center">
             <h1>Add items</h1>
-            <div className="w-[400px]  border flex flex-col">
+            <div className="w-[400px]  border flex flex-col ">
                 <input type="text" placeholder="Product Key"/>
                 <input type="text" placeholder="Product Name"/>
                 <input type="text" placeholder="Product Price"/>
