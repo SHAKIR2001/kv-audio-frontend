2341837fe195b49322799b10574a44dc3ec1247f
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-17 12:38:06 +0530

    add CiCirclePlus icon to AdminItemsPage component 👍

--- src/pages/admin/adminItemsPage.jsx
+++ src/pages/admin/adminItemsPage.jsx
@@ -1,7 +1,10 @@
+import { CiCirclePlus } from "react-icons/ci";
 export default function AdminItemsPage(){
 
     return(
         <div className="w-full h-full bg-amber-200">
+            <div><CiCirclePlus /></div>
+
             
             
         
