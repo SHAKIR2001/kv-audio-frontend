80819bc8cd3d5409a4d01b65be9b2c22a2bd50a3
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-17 12:55:03 +0530

    adjust icon positioning on AdminItemsPage for better visibility 😁

--- src/pages/admin/adminItemsPage.jsx
+++ src/pages/admin/adminItemsPage.jsx
@@ -3,7 +3,7 @@ export default function AdminItemsPage(){
 
     return(
         <div className="w-full h-full relative">
-            <CiCirclePlus className="text-[100px] absolute bottom-15 right-20"/>
+            <CiCirclePlus className="text-[100px] absolute bottom-2 right-2 hover:text-red-900 cursor-pointer "/>
 
             
             
