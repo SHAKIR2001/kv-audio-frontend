6574edda3fcef032f346fc6d2b4569adc7a5880b
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-17 15:08:59 +0530

    fix the spelling mistake 😣

--- src/pages/admin/addProductPage.jsx
+++ src/pages/admin/addProductPage.jsx
@@ -3,6 +3,7 @@ export default function AddProduct(){
     const[productKey,setProductKey] = useState("");
     const[productName,setproductName] = useState("");
     const[productPrice,setproductPrice] = useState("");
+    const[productCatagorie,setProductCatagorie] = useState("");
     const[productDescription,setproductDescription] = useState("");
     const[productDimensions,setproductDimensions] = useState("");
 
@@ -14,11 +15,11 @@ export default function AddProduct(){
         <div className="w-full h-full flex flex-col items-center">
             <h1>Add items</h1>
             <div className="w-[400px]  border flex flex-col items-center ">
-                <input type="text" placeholder="Product Key"/>
+                <input  type="text" onChange={ (e)=>{setProductKey(e.target.value)}} value={productKey} placeholder="Product Key"/>
                 <input type="text" placeholder="Product Name"/>
                 <input type="text" placeholder="Product Price"/>
 
-                <select>
+                <select onChange={(e)=>{setProductCatagorie(e.target.value)}} value={productCatagorie}>
                     <option key="audio">Audio</option>
                     <option key="lights">Lights</option>
                     
