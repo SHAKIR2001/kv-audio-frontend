ef4b721ef2cc7175a44ed1217da4019bd82d6c3f
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-20 11:31:31 +0530

    fix: update title in UpdateProduct component from 'Add New Product' to 'Update Item' 😒

--- src/pages/admin/updateItemPage.jsx
+++ src/pages/admin/updateItemPage.jsx
@@ -64,7 +64,7 @@ export default function UpdateProduct() {
 
         {/* Title */}
         <h1 className="text-2xl font-bold text-gray-800 text-center mb-6">
-          Add New Product
+          Update Item
         </h1>
 
         {/* Form */}
