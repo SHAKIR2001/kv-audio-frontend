ba5a67ddfb4acb7c7cbf8c783828276e1398c5e4
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-20 10:14:21 +0530

    fix: update border style and shape for visual consistency in AdminItemsPage component😁

--- src/pages/admin/adminItemsPage.jsx
+++ src/pages/admin/adminItemsPage.jsx
@@ -54,7 +54,7 @@ export default function AdminItemsPage() {
 
   return (
     <div className="w-full min-h-screen bg-gray-100 p-6 relative flex flex-col  items-center ">
-      <div className="border-4 my-4 w-[100px] h-[100px] bg-red-900"></div>
+      <div className="border-4 my-4 rounded-full w-[100px] h-[100px]  border-b-green-500"></div>
       {/* Page Title */}
       <h1 className="text-2xl font-bold text-gray-800 mb-6">
         Product Management
