b222ad5d2b78eea9f1ea0f9c6876eea3ef8ee3ad
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 11:54:13 +0530

    feat: add RegisterPage route to the application 😪

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/home/homePage';
 import Testing from './components/testing';
 import LoginPage from './pages/login/login';
 import toast, { Toaster } from 'react-hot-toast';
+import RegisterPage from './pages/register/register';
 function App() {
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/admin/*" element={<AdminPage/>}/>
         <Route path="/*" element={<HomePage/>}/> 
         <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
         
         
 
