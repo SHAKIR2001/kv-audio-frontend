a5e807c83694bee32bd74daab3e3999f4bba1b9e
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 15:40:21 +0530

    update API call handling in Items component to set state and items correctly 😋

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -8,8 +8,9 @@ export default function Items(){
     useEffect( ()=>{
         
       if(state == "loading"){
-          axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`).then( (res)=>{ //prodcuts can be view by anyone thats why i didnt get any token 
-          console.log(res)
+           axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`).then( (res)=>{ //prodcuts can be view by anyone thats why i didnt get any token 
+           setItems(res.data)
+           setState("success")
         }).catch( (err)=>{
           console.log(err?.responce?.data?.error || "Erorr occured")
         }) 
