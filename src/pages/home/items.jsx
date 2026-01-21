0d6a19fa91ec3dc05198202451e1fa9660fc4517
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 14:52:24 +0530

    fix: correct API response handling in Items component 😁

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -2,10 +2,10 @@ import { useEffect } from "react"
 export default function Items(){
 
     useEffect( ()=>{
-        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`) //prodcuts can be view by anyone thats why i didnt get any token 
-    },[]).then( (res)=>{
-        console.log(res.data)
-    })
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`).then( (res)=>{ //prodcuts can be view by anyone thats why i didnt get any token 
+        console.log(res)
+    }) 
+    },[])
 
 
 
