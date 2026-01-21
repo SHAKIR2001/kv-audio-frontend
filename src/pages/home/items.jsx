b116e442ab1828ef6673245c203e3f1d9e92a694
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 15:46:41 +0530

    add missing newline and clean up Items component structure 😎

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -9,6 +9,7 @@ export default function Items(){
     useEffect( ()=>{
         
       if(state == "loading"){
+
            axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`).then( (res)=>{ //prodcuts can be view by anyone thats why i didnt get any token 
            setItems(res.data)
            setState("success")
@@ -25,6 +26,8 @@ export default function Items(){
 
 
     return(
-        <h1>Items</h1>
+        <div className="w-full h-full ">
+
+        </div>
     )
 }
