65377552ea06aca54a6ee7c79ac243d31fe8d154
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 14:14:57 +0530

    fix: update success message to include user's last name on login 😒

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -20,7 +20,7 @@ export default function LoginPage(){
         }).then(
             (res)=>{
                 console.log(res)
-                toast.success("Login Success, Welcome "+res.data.user.firstName)
+                toast.success("Login Success, Welcome "+res.data.user.lastName)
                 const user = res.data.user //identify this from browser console(res.data.user)
                 localStorage.setItem("token", res.data.token) //store the key and value in the  cashe table(token)
                  
