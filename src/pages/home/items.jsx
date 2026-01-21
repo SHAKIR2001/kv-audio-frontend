c3129bd24b0ca87044bcf2db3228b62b7a6a1923
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 14:43:49 +0530

    fix: clean up formatting in Items component 😊

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -2,8 +2,12 @@ import { useEffect } from "react"
 export default function Items(){
 
     useEffect( ()=>{
-
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`) //prodcuts can be view by anyone thats why i didnt get any token 
     },[])
+
+
+
+
     return(
         <h1>Items</h1>
     )
