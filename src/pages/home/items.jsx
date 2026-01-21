b2ef8fbaa9ff20f67042ac963b5cbfa8b92caefd
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 14:55:33 +0530

    fix: improve error handling in Items component API call😪

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react"
+import axios from "axios"
 export default function Items(){
 
     useEffect( ()=>{
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`).then( (res)=>{ //prodcuts can be view by anyone thats why i didnt get any token 
         console.log(res)
+    }).catch( (err)=>{
+        console.log(err?.responce?.data?.error || "Erorr occured")
     }) 
     },[])
 
