e950a6c0bf4e512151b7fe8c31e413f4a1beecec
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 12:07:10 +0530

    fix: remove placeholder text from RegisterPage component 🫣

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -2,7 +2,9 @@ import "./register.css"
 export default function RegisterPage(){
     return(
         <div className="w-full h-screen bg-picture flex justify-center items-center ">
-            Register Page
+            <div className="w-[400px] h-[600px] backdrop-blur-xl">
+
+            </div>
         </div>
     )
 }
