a8dbe7ac1e0796628cb6b9f4cf5ce0f5bd565f2a
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-21 14:51:21 +0530

    fix: add handling for API response in Items component😋

--- src/pages/home/items.jsx
+++ src/pages/home/items.jsx
@@ -3,7 +3,9 @@ export default function Items(){
 
     useEffect( ()=>{
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products`) //prodcuts can be view by anyone thats why i didnt get any token 
-    },[])
+    },[]).then( (res)=>{
+        console.log(res.data)
+    })
 
 
 
