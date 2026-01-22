06f7d93ef17646e74f8edde227e602a1199bc1e1
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-22 14:54:08 +0530

    add background color to Header component and ensure text color is white 🫣

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Header(){
     return(
-        <header className="w-full h-[100px] shadow-xl flex justify-center items-center relative">
+        <header className="w-full bg-accent h-[100px] shadow-xl flex justify-center items-center relative">
             <img src="logo.png" className="w-[100px] h-[100px] object-cover absolute left-1 border-[3px] rounded-full border-gray-200" /> 
-            <Link to="/" className=" text-[25px] font-bold m-1">Home</Link>
-            <Link to="/contact" className=" text-[25px] font-bold m-1">Contact</Link>
-            <Link to="/gallery" className=" text-[25px] font-bold m-1">Gallery</Link>
-            <Link to="/items" className=" text-[25px] font-bold m-1">Items</Link>
+            <Link to="/" className="text-white text-[25px] font-bold m-1">Home</Link>
+            <Link to="/contact" className="text-white  text-[25px] font-bold m-1">Contact</Link>
+            <Link to="/gallery" className="text-white  text-[25px] font-bold m-1">Gallery</Link>
+            <Link to="/items" className="text-white  text-[25px] font-bold m-1">Items</Link>
 
            
 
