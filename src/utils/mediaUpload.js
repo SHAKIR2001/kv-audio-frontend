81e56e861becaa5256bbad28ef851ef740e8a709
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-23 10:51:33 +0530

    fix: correct variable name and initialize Supabase client in mediaUpload utility 😊😊

--- src/utils/mediaUpload.js
+++ src/utils/mediaUpload.js
@@ -1,2 +1,5 @@
+
 const anon_key = "sb_publishable_YVS8QAiPk-iDM1QHIa9qFQ_yslPD0ka"
-const superbase_url = "https://wzqkwvblzpxdxfmjstie.supabase.co"
+const supabase_url = "https://wzqkwvblzpxdxfmjstie.supabase.co"
+
+const supabase = createClient()
