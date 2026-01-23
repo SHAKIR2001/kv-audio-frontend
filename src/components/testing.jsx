d391b264ed904f528242bf8b2904a3d4fca664e2
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-23 09:48:08 +0530

    add upload button to Testing component 😊

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -7,6 +7,7 @@ export default function Testing(){
         <div className="w-full h-screen flex justify-center items-center flex-col">
             
             <input type="file"/>
+            <button className="bg-blue-500 border-2 w-[80px] h-[50px] rounded-2xl mr-20 cursor-pointer">Upload</button>
    
 
         </div>
