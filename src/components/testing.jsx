82b9828cd406fe7038ca741f8b3791e2786d351b
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-23 09:49:48 +0530

    refactor Testing component to remove unused state and adjust button height 😊

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react"
 export default function Testing(){
-    const [count,setCount] = useState(0)
-    const [items, setItems] = useState("Banana")
+    const [file,setFile] = useState(null);
 
     return(
         <div className="w-full h-screen flex justify-center items-center flex-col">
             
             <input type="file"/>
-            <button className="bg-blue-500 border-2 w-[80px] h-[50px] rounded-2xl mr-20 cursor-pointer">Upload</button>
+            <button className="bg-blue-500 border-2 w-[80px] h-[40px] rounded-2xl mr-20 cursor-pointer">Upload</button>
    
 
         </div>
