31fe5a9163257814770cf54f94f598fee6015eeb
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-23 09:40:38 +0530

    remove unused state and buttons from Testing component 😪

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -6,41 +6,8 @@ export default function Testing(){
     return(
         <div className="w-full h-screen flex justify-center items-center flex-col">
             
-            <h1 className="text-4xl m-2">{count + " " +items+"s" }</h1>
-            <button className="cursor-pointer border rounded bg-red-500 p-1 m-1" onClick={()=>{
-                const newCount = count + 1
-                setCount(newCount)
-            }}>Count</button>
+   
 
-            <div className="flex w-[350px] justify-between items-center mt-3">
-
-            <button className="cursor-pointer border rounded bg-yellow-500 p-1 m-1" onClick={()=>{
-            
-            setItems("Banana")
-
-            }}>Banana</button>
-
-
-            <button className="cursor-pointer border rounded bg-red-500 p-1 m-1" onClick={()=>{
-            
-            setItems("Apple")
-
-            }}>Apple</button>
-
-            <button className="cursor-pointer border rounded bg-orange-500 p-1 m-1" onClick={()=>{
-            
-            setItems("Orange")
-
-            }}>Orange</button>
-
-
-            <button className="cursor-pointer border rounded bg-amber-300 p-1 m-1" onClick={()=>{
-            
-            setItems("Other")
-
-            }}>Other</button>
-
-            </div>
         </div>
          
     )
