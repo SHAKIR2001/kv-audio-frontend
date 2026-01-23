772f28c4046f65e7cce3b3e019d4b1cbefc91252
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-23 11:44:02 +0530

    update mediaUpload function to generate unique file names and retrieve public URL correctly 😎

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -6,11 +6,15 @@ const supabase_url = "https://wzqkwvblzpxdxfmjstie.supabase.co"
 const supabase = createClient(supabase_url, anon_key )
 
 export default function mediaUpload(file){
-    supabase.storage.from("images").upload(file.name, file, { //images : bucket name ; "filr : parameter name from the funtion mediaUpload; file.name : supabase il kurippitta file save aha wendiya name"
+    const timesStamp = new data().getTime(); //get the current time
+    const fileName = timesStamp+file.name; //in supabase we cant save files with the same name so we add currentTime infront of the file name
+
+    supabase.storage.from("images").upload(fileName, file, { //images : bucket name ; "filr : parameter name from the funtion mediaUpload; wendiya name"
         cacheControl : '3600',
         upsert: false
     }).then( (res)=>{
-        const publicUrl = supabase.storage.from("images").getPublicUrl(file.name).data.publicUrl; //images : buacket name ; file.name: related file/image name;   upload ahina image in Url ei eduththal 
+
+        const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl; //images : buacket name ; file.name: related file/image name;   upload ahina image in Url ei eduththal 
         console.log(publicUrl)
     })
 }
