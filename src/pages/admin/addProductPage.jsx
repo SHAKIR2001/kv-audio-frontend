3d88b3b43eafc873a824c1f521ad40bdf298907f
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-25 14:39:24 +0530

    fix: remove duplicate toast import in addProductPage.jsx 😁

--- src/pages/admin/addProductPage.jsx
+++ src/pages/admin/addProductPage.jsx
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import mediaUpload from "../../utils/mediaUpload";
-import toast from "react-hot-toast";
+
 
 export default function AddProduct() {
   const [productKey, setProductKey] = useState("");
