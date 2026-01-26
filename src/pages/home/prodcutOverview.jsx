fb11f802d90aa17fe3c6fb555f121830d2a532a5
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-26 22:14:20 +0530

    fix: add state management for loading status and product in ProductOverview component 😊

--- src/pages/home/prodcutOverview.jsx
+++ src/pages/home/prodcutOverview.jsx
@@ -2,7 +2,10 @@ import { useParams } from "react-router-dom";
 export default function ProductOverview(){
     const params = useParams();
     const key = params.key;
-    
+
+    const [loadingStatus, setLoadingStatus] = useState("loading");
+    const [product, setProduct] = useState({});
+
 
     return(
         <dev>Prodcut Overview</dev>
