69a4fbe2be06e34ae93cfee4824ff8efca9aded8
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-26 22:16:42 +0530

    fix: add useEffect and useState imports in ProductOverview component 🫣

--- src/pages/home/prodcutOverview.jsx
+++ src/pages/home/prodcutOverview.jsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom";
+import { useEffect,useState } from "react";
 export default function ProductOverview(){
     const params = useParams();
     const key = params.key;
 
     const [loadingStatus, setLoadingStatus] = useState("loading");
     const [product, setProduct] = useState({});
+
+    useEffect( ()=>{
+        
+    },[])
 
 
     return(
