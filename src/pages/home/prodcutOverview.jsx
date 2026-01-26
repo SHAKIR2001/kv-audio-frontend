dc687afc6e2df003ecce0ebf8ea86f280c7b3084
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-26 22:10:27 +0530

    fix: add console log for params in ProductOverview component😊

--- src/pages/home/prodcutOverview.jsx
+++ src/pages/home/prodcutOverview.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 export default function ProductOverview(){
     const params = useParams();
+    console.log(params)
     return(
         <dev>Prodcut Overview</dev>
     )
