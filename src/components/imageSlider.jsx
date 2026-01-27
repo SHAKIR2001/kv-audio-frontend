697236f7cb2a33aea6d4fa9b5fd07654c8836b54
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-27 13:28:56 +0530

    fix: update ImageSlider layout and image display logic 😊

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -1,8 +1,19 @@
 export default function ImageSlider(props){
     const images =  props.images
     return(
-        <div className="w-full h-full flex flex-col items-center bg-red-300">
-           <img src={images[0]} alt="product" className="w-[90%] h-[50%] object-cover"/>
+        <div className="w-full h-full flex flex-col items-center ">
+           {images[0] != null && <img src={images[0]} alt="product" className="w-full h-[450px] 
+           object-cover"/>}
+        <div className="w-full h-[150px] flex justify-center mt-[20px]">
+                {
+                    images.map( (image,index)=>{
+                        return <img key={index} src={image} alt="Product" className="w-[100px] mr-[2px] h-[100px] object-cover cursor-pointer "/>
+                    })
+                }
+        </div>
+
+
+
         </div>
     )
 }
