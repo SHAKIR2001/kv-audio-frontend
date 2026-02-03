c26335788330e84a08befbc69eb54b41a8dc2468
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-03 10:18:26 +0530

    fix: correct initialization of startingDate in LoadCart function 👍

--- src/utils/cart.jsx
+++ src/utils/cart.jsx
@@ -5,7 +5,7 @@ export function LoadCart (){  //this function get related cart details that is s
         cart = {
             orderedItems : [],
             days : 1,
-            startingDate : new Date().toISOString,
+            startingDate : new Date()
             
 
             
@@ -14,7 +14,7 @@ export function LoadCart (){  //this function get related cart details that is s
 
 }
 
-export function formatDate(date) {
+export function formatDate(date) {  //This function is used to convert a JavaScript Date object into a clean, readable date string in the format : YYYY-MM-DD
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure two digits
   const day = String(date.getDate()).padStart(2, '0');       // Ensure two digits
