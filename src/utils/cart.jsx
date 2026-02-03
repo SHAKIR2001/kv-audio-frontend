57537b3ca8838e2c1765f7924d2fb9e236f307c2
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-03 10:09:13 +0530

    fix: improve LoadCart function documentation and structure 😊

--- src/utils/cart.jsx
+++ src/utils/cart.jsx
@@ -1,3 +1,8 @@
-export function LoadCart (){
-    
+export function LoadCart (){  //this function get related cart details that is stored in localStorage(browser)
+    const cart = localStorage.getItem("cart")
+
+    if(cart == null){ //new user / or he dont have put anything in the cart
+
+    }
+
 }
