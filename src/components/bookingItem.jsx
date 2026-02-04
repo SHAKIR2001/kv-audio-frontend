157663fb91cf3754828fc81ed09662be84a519fc
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-04 10:42:31 +0530

    fix: update BookingItems component to include refresh prop and handle error by removing item from cart😎

--- src/components/bookingItem.jsx
+++ src/components/bookingItem.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { removeFromCart } from "../utils/cart";
 export default function BookingItems(props){
-    const {itemKey ,quantity} = props;
+    const {itemKey ,quantity, refresh} = props; //refresh use to to get loadCart from cart.jsx
     const [item, setItem] = useState(null);
     const [status, setStatus] = useState("loading"); //loading / error / success
 
@@ -13,6 +14,7 @@ export default function BookingItems(props){
         }).catch( (err)=>{
             console.log(err);
             setStatus("error");
+            removeFromCart(itemKey) //if error comes we need to remove the perticuller item from the cart
         })      
         }
 
