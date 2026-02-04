5c6b20b43134143bcab8bd375ce75a4762b014b4
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-04 12:34:53 +0530

    fix: enhance BookingPage and ProductOverview components with date formatting and toast notifications 😪

--- src/pages/home/bookingPage.jsx
+++ src/pages/home/bookingPage.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { loadCart } from "../../utils/cart";
+import { loadCart ,formatDate } from "../../utils/cart";
 import BookingItems from "../../components/bookingItem";
 
 export default function BookingPage(){
 
     const [cart, setCart] = useState(loadCart()); //get thease from cart.jsx (browser storage)
+    const today = formatDate(new Date()); //get this formatDate function from cart.js
+    const tommorow = formatDate(new Date(Date.now() + 24 * 60 * 60 * 1000)); 
 
     function reloadCart(){  //this function reload the page using loadCart
         setCart(loadCart());
@@ -20,6 +22,9 @@ export default function BookingPage(){
                 return <BookingItems itemKey={item.key} key={item.key} quantity={item.quantity} refresh={reloadCart}/> //send the reloadCart function to bookingItem.jsx through props //2nd key used as a unique key need when using map
             })
            } 
+        </div>
+        <div className="w-full flex justify-center ">
+
         </div>
         
 
