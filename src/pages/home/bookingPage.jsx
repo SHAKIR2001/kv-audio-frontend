7a866b2f015129295aba5f50234d244c72c332e3
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-04 10:46:24 +0530

    fix: add refresh prop to BookingItems component for cart reloading functionality 😒

--- src/pages/home/bookingPage.jsx
+++ src/pages/home/bookingPage.jsx
@@ -6,7 +6,7 @@ export default function BookingPage(){
 
     const [cart, setCart] = useState(loadCart()); //get thease from cart.jsx (browser storage)
 
-    function reloadCart(){
+    function reloadCart(){  //this function reload the page using loadCart
         setCart(loadCart());
     }
 
@@ -17,7 +17,7 @@ export default function BookingPage(){
         <div className="w-full flex flex-col items-center" >
            {
             cart.orderedItems.map( (item)=>{
-                return <BookingItems itemKey={item.key} quantity={item.quantity}/>
+                return <BookingItems itemKey={item.key} quantity={item.quantity} refresh={reloadCart}/> //send the reloadCart function to bookingItem.jsx through props
             })
            } 
         </div>
