cc67561d929acda230d55a428d1b540ef8527709
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-09 15:23:17 +0530

    refactor: comment out state initialization and loading logic in AdminBookingPage component 😋

--- src/pages/admin/adminBokkingPage.jsx
+++ src/pages/admin/adminBokkingPage.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 export default function AdminBookingPage(){
 
-    const [bookings, setBooking] = useState([]);
-    const [loading, setLoading] = useState("loading");
+   /* const [bookings, setBooking] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( ()=>{
+
+
+    }, [loading]) */
 
     return(
         <div>Bookings</div>
