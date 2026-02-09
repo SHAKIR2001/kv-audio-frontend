1f0916774b4d6d7b8118e8aedb16642a6dd1d3a7
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-09 11:27:32 +0530

    feat: initialize state and loading logic in AdminUsersPage component 😎

--- src/pages/admin/adminUsersPage.jsx
+++ src/pages/admin/adminUsersPage.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
 export default function AdminUsersPage(){
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( ()=>{
+        if(loading){
+            const token = localStorage.getItem("token")
+        }
+
+    },[loading])
+
     return(
         <div>Admin Users</div>
     )
