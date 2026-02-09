d6a5d7d148c0e18503c46cf703149daaff5a4064
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-09 15:00:42 +0530

    feat: initialize state for bookings and loading in AdminBookingPage component 😊

--- src/pages/admin/adminBokkingPage.jsx
+++ src/pages/admin/adminBokkingPage.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 export default function AdminBookingPage(){
+
+    const [bookings, setBooking] = useState([]);
+    const [loading, setLoading] = useState("loading");
+
     return(
         <div>Bookings</div>
     )
