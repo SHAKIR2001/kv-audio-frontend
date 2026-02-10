bb06ae2a6ba08cb42620529c2ac13e47d7ffd32e
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-02-10 17:14:27 +0530

    feat: conditionally render routes based on user validation in AdminPage component 👍

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -104,13 +104,13 @@ export default function AdminPage(props){
         {/* Routed pages container */}
         <div className="p-4 md:p-6">
           <div className="max-w-7xl mx-auto">
-            <Routes path="/*">
+            {userValidated &&<Routes path="/*">
               <Route path="/orders" element={<AdminOrderPage/>}/>
               <Route path="/items" element={<AdminItemsPage/>}/>
               <Route path="/items/add" element={<AddProduct/>} />
               <Route path="/items/edit" element={<UpdateProduct/>} />
               <Route path="/users" element={<AdminUsersPage/>} />
-            </Routes>
+            </Routes>}
           </div>
         </div>
       </div>
